53837b4066a454d19f1325f9a10de127bc79c698
Sreeram Ramakrishnan <sriram3rhyme@gmail.com>
2018-10-18 22:26:23 -0400

    Added comments

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -1,38 +1,37 @@
 //
-//  ViewController.swift
-//  ShoppingList
+//  File Name: ShoppingList
 //
 //  Created by Sreeram Ramakrishnan on 2018-10-18.
-//  Copyright © 2018 Centennial College. All rights reserved.
+//  Student ID: 301042442
 //
 
 import UIKit
 
 class ViewController: UIViewController{
     
-    @IBOutlet var TextFields: [UITextField]!
+    @IBOutlet var TextFields: [UITextField]!  //Textfield Outlet Collection
     
-    @IBAction func Reset(_ sender: UIButton) {
-        TextFields.forEach({ $0.text = "" })
+    @IBAction func Reset(_ sender: UIButton) { //Reset Button Action
+        TextFields.forEach({ $0.text = "" })   //changes text of all text fields to nil
         
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))  //used to hide keyboard when clicked anywhere on screen
         tap.cancelsTouchesInView = false
         self.view.addGestureRecognizer(tap)
     }
     
-    @IBAction func color1(_ sender: UIBarButtonItem) {
+    @IBAction func color1(_ sender: UIBarButtonItem) { //Color 1 in Color Swatch
         UIView.animate(withDuration: 1, animations: {
-            self.view.backgroundColor = UIColor.green }, completion: nil)
+            self.view.backgroundColor = UIColor.green }, completion: nil)  //switch background color to green
     }
     
-    @IBAction func gray(_ sender: UIBarButtonItem) {
+    @IBAction func gray(_ sender: UIBarButtonItem) {  //Color 2 in Color Swatch
         UIView.animate(withDuration: 1, animations: {
-            self.view.backgroundColor = UIColor.gray}, completion: nil)
+            self.view.backgroundColor = UIColor.gray}, completion: nil)  //switch background color to gray
     }
     
 
